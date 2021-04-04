9710b33dd07e1ed27895592d29d10beadcd583ec
jgbroz <george_ashcroft@live.com.au>
2021-04-04 14:38:34 +0000

    This initializes the initial database

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,22 @@
 # Central System Web Application Process
 
 from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="jgbroz",
+    password="Acs3302021",
+    hostname="jgbroz.mysql.pythonanywhere-services.com",
+    databasename="jgbroz$CentralSystem",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
