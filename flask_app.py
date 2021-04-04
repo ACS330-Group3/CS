168cf44ab04e9b6ce14d8fb5d0f5e4612adbe89c
jgbroz <george_ashcroft@live.com.au>
2021-04-04 13:48:52 +0000

    Landing Page Template

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,12 @@
 
 # Central System Web Application Process
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
-def hello_world():
-    return 'Testing out Functions'
+@app.route("/")
+def index():
+    return render_template("main_page.html")
 
