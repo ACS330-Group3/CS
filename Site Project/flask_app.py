7dab1083bfbdb87f2b9601dd289b5beb3b71fc2f
jgbroz <george_ashcroft@live.com.au>
2021-04-07 12:29:26 +0000

    Upload method for Images and string passing

--- Site Project/flask_app.py
+++ Site Project/flask_app.py
@@ -1,9 +1,10 @@
 
 # Central System Web Application Process
 
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, Response
 from flask_sqlalchemy import SQLAlchemy #Imports Necessary Information
 import datetime
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -38,10 +39,11 @@ class Product(db.Model):
     __tablename__ = "productDetails"
 
     productID = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(75))
-    password = db.Column(db.String(70))
-    imageFile = db.Column(db.String(40), nullable=False, default='default.jpg')
-    date = db.Column(db.DateTime)
+    user = db.Column(db.String(75), default='No Input')
+    password = db.Column(db.String(70), default='No Input')
+    img = db.Column(db.Text, unique=True, default='No Input')
+    imgName = db.Column(db.Text, nullable = False, default='No Input')
+    mimeType = db.Column(db.Text, nullable = False, default='No Input')
     status = db.Column(db.String(100), nullable=False, default='Waiting for Pick Up')
 
 
@@ -58,13 +60,59 @@ def index():
     return redirect(url_for('index'))
 #Creates simple landing page with commenting section
 
+
 @app.route("/query")
 def query():
-
     args = request.args.get("a")
     comment = Comment(content=args)
     db.session.add(comment)
     db.session.commit()
     print(args)
     return redirect(url_for('index'))
+
+
+@app.route("/apptest")
+def apptest():
+    userIn = request.args.get("userIn")
+    passwordIn = request.args.get("passIn")
+
+
+    upload = Product(user=userIn, password=passwordIn)
+    db.session.add(upload)
+    db.session.commit()
+    return redirect(url_for('index'))
 #Apply a method to create query strings and use data to import info
+
+
+
+
+#Upload Image Region
+@app.route("/upload", methods=["GET","POST"])
+def upload():
+    if request.method == "GET":
+        return render_template("upload_files.html")
+    pic = request.files['pic']
+    if not pic:
+        return 'No pic Loaded', 400
+
+    filename = secure_filename(pic.filename)
+    mime = pic.mimetype
+    imageData = pic.read()
+
+    if not filename or not mime:
+        return 'Bad upload!', 400
+
+    img = Product(user="default", password="default", img=imageData, imgName=filename, mimeType=mime)
+    db.session.add(img)
+    db.session.commit()
+    #return imageData
+    return Response(imageData, mimetype=img.mimeType)
+    #return Response(img.img, mimetype=img.mimeType)
+
+@app.route('/<int:id>')
+def get_img(id):
+    img = Product.query.filter_by(productID=id).first()
+    if not img:
+        return 'Img Not Found!', 404
+
+    return Response(img.img, mimetype=img.mimeType)
