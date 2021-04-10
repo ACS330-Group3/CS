ecad536610869883129e19fd1892bc5b94ef9276
jgbroz <george_ashcroft@live.com.au>
2021-04-10 12:05:41 +0100

    Method to retrieve column data as requested

--- CS Logic/Central System.py
+++ CS Logic/Central System.py
@@ -6,6 +6,12 @@ Created on Sat Apr 10 09:18:37 2021
 """
 
 import downloadImage
+import retrieveInfo
 
 #Callable method to retrieve an image by Primary Key Insert where needed
-test = downloadImage.retrieveImage(17)
+#testImage = downloadImage.retrieveImage(17)
+
+#Test Sequence to retrieve column data from database
+testInfo = retrieveInfo.retrieveCol(13, 'user')
+
+print(testInfo)
