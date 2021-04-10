2b2592fde374cb10b33b6d4c556db11f8730e5eb
jgbroz <george_ashcroft@live.com.au>
2021-04-10 10:39:06 +0100

    Beginning function Development initial Image Downlaod

---
+++ CS Logic/Central System.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 09:18:37 2021
+
+@author: georg
+"""
+
+import downloadImage
+
+#Callable method to retrieve an image by Primary Key Insert where needed
+test = downloadImage.retrieveImage(17)
---
+++ CS Logic/downloadImage.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 07:02:29 2021
+
+@author: georg
+"""
+#Imports necessary packages
+import sshtunnel
+import mysql.connector
+import requests
+import os
+
+#Defines the function to call outside of the system
+def retrieveImage(Id):
+    #Establishes secure tunnel connection config settings
+    sshtunnel.SSH_TIMEOUT = 5.0
+    sshtunnel.TUNNEL_TIMEOUT = 5.0
+    
+    #Connects with database through SSH tunnel hosted on public Linux Setup
+    try:
+        with sshtunnel.SSHTunnelForwarder(
+            ('ssh.pythonanywhere.com'),
+            ssh_username='jgbroz', ssh_password='Sly!25321',
+            remote_bind_address=('jgbroz.mysql.pythonanywhere-services.com', 3306)
+        ) as tunnel:
+            db = mysql.connector.connect(
+                user='jgbroz', password='Acs3302021',
+                host='127.0.0.1', port=tunnel.local_bind_port,
+                database='jgbroz$CentralSystem',
+            )
+            
+            #Change Path when integrating
+            savePath = 'D:/ACS330 CS Code/Central System/CS/CS Logic/Retrieved Images'
+            
+            #Saves the passed in Id
+            productID = Id
+            #Begin Query Process
+            mycursor = db.cursor()
+            #select image Column from Database Table
+            mycursor.execute("SELECT img FROM productDetails WHERE productID = "+ str(productID))
+            
+            #Store the first result in a variable
+            row = mycursor.fetchone()
+            
+            #Join File paths to one another when saving image
+            completePath = os.path.join(savePath, row[0])
+            #Join Image name to expected url to access it
+            setUrl = 'http://jgbroz.pythonanywhere.com/uploads/'+ row[0]
+            
+            #print (setUrl) Prints the full URL (Testing)
+            #Downlaods image using Requests tree
+            r = requests.get(setUrl, allow_redirects=True)
+            #Opens a new file to save the data to.
+            open(completePath, 'wb').write(r.content)
+            #Closes the database 
+            db.close()
+            #returns a success if successful
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        return ('Fail')
+    finally:
+        if db:
+            print('Connection still open. Will close now:')
+            print('Succesful Image Acquisition')
+            db.close()
+            #returns a success if successful
+            return ('Success')
+        print('All Connections closed')
+        return ('Success')
