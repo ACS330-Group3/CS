f7d7da2aee90f8447714a894f43687ff8e424c89
jgbroz <george_ashcroft@live.com.au>
2021-04-13 07:53:19 +0100

    New function produced an to do list added

--- CS Logic/Central System.py
+++ CS Logic/Central System.py
@@ -8,20 +8,25 @@ Created on Sat Apr 10 09:18:37 2021
 import downloadImage
 import retrieveInfo
 import updateStatus
-
+import checkDB
+#import checkRFID
 
 
 """
 Dummy Function List that needs doing:
     Priority:
         GENERAL LOGIC FRAME NEEDS TO BE ADDED HERE
-        checkRFID
-        checkStations
-        sendToStation (may need extra):
+        
+        checkRFID (Checks for new RFID pings to update the status)
+        
+        checkStations (Checks for inputs from stations research needed)
+            checkOmni
+            checkDS
+            checkQC
+        sendToStation (Functions to provide instruction):
             directOmni
             directDS
             directQC
-
     
     Extras:
         ceaseProduction
@@ -29,13 +34,21 @@ Dummy Function List that needs doing:
     
     compeleted:
         retrieveInfo
-        downloadImage(change to download 6 images)
+        downloadImage(changed to download 6 images)
         updateStatus     
+        checkDB (Checks for new arrivals based on status)
     
 """
 
+#Error = False
+
+
+#Callable method to retrieve any product ID needing pick up
+#testDBCheck = checkDB.checkPickUp()
+#print(testDBCheck)
+
 #Callable method to retrieve an image by Primary Key Insert where needed
-#testImage = downloadImage.retrieveImage(5)
+#testImage = downloadImage.retrieveImage(10)
 #print(testImage)
 
 #Test Sequence to retrieve column data from database
---
+++ CS Logic/checkDB.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 12 08:20:01 2021
+
+@author: georg
+"""
+
+#Imports necessary packages
+import sshtunnel
+import mysql.connector
+
+def checkPickUp():
+    try:
+            with sshtunnel.SSHTunnelForwarder(
+                ('ssh.pythonanywhere.com'),
+                ssh_username='jgbroz', ssh_password='Sly!25321',
+                remote_bind_address=('jgbroz.mysql.pythonanywhere-services.com', 3306)
+            ) as tunnel:
+                db = mysql.connector.connect(
+                    user='jgbroz', password='Acs3302021',
+                    host='127.0.0.1', port=tunnel.local_bind_port,
+                    database='jgbroz$CentralSystem',
+                )
+                
+
+                #Construct Query String
+                queryString = "SELECT productID FROM productDetails WHERE status = 'Waiting for Pick Up'"
+                
+                
+                #Begin Query Process
+                mycursor = db.cursor()
+                #select image Column from Database Table
+                mycursor.execute(queryString)
+
+                #Store the first result in a variable
+                row = mycursor.fetchone()
+                db.close()
+                #returns a success if successful
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        return ('Fail')
+    finally:
+        if db:
+            print('Connection still open. Will close now:')
+            print('Succesful Image Acquisition')
+            db.close()
+            #returns a success if successful
+            return (row[0])
+        print('All Connections closed')
+        return (row[0])
