2f803d22222a129b43e814946b8e0e09a7f21883
jgbroz <george_ashcroft@live.com.au>
2021-04-15 17:03:02 +0400

    Converted to 2.7 for ROS integration and developed a venv

---
+++ PyCharm_Conversion/Central System.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 09:18:37 2021
+Converted to python 2.7
+@author: georg
+"""
+import time
+import downloadImage
+import retrieveInfo
+import updateStatus
+import checkDB
+
+#import checkRFID
+
+
+"""
+Dummy Function List that needs doing:
+    Priority:
+        GENERAL LOGIC FRAME NEEDS TO BE ADDED HERE
+        
+        checkRFID (Checks for new RFID pings to update the status)
+
+        checkStations (Checks for inputs from stations research needed)
+            checkOmni
+            checkDS
+            checkQC
+        sendToStation (Functions to provide instruction):
+            directOmni
+            directDS
+            directQC
+    
+    Extras:
+        ceaseProduction
+        checkErrors
+    
+    compeleted:
+        retrieveInfo
+        downloadImage(changed to download 6 images)
+        updateStatus     
+        checkDB (Checks for new arrivals based on status)
+    
+
+Expected Sequence of Status for a succesful Run:
+Waiting for Pick Up -> Transporting to DS -> Drawing Images -> Transporting to QC
+-> Performing QC -> Passed QC Transport to Packaging
+
+Possible functions that need to be made: 
+
+Tasks that need to be done between 'Waiting for Pick up' and 'Trasnporting to DS':
+    Assign Cube wth corresponding productID
+    Inform Omniplatform that cube needs picking up
+    Tell it to go to DS
+    Update Database Status
+
+Tasks that need to be done between 'Transporting to DS' and 'Drawing Images':
+    recieve RFID ping from Cube corresponding to product ID from DS
+    Download Images corresponding to Product ID
+    Inform DS that the Cube needs Picking up and Drawing On.
+    Update Database Status
+
+Tasks that need to be done between 'Drawing Images' and 'Transporting to QC':
+    recieve message from DS saying it's completed
+    Have the Cube placed back on omni (may not be our job)
+    Inform Omni that the cube has been placed and needs to be taken to QC
+    Update Database Status
+    
+Tasks that need to be done between 'Transporting to QC' and 'Performing QC':
+    recieve RFID ping from Cube corresponding to product ID from QC
+    Have the OMNI place cube on QC
+    Inform QC that cube has been placed pass original images 
+    Update Database Status
+
+Tasks that need to be done between 'Transporting to QC' and 'Performing QC':
+    recieve message from QC saying it's completed and the match
+    Inform Omni that it has the cube 
+    Inform Omni to to packaging
+    Update Database Status
+    Complete
+"""
+
+#Error = False
+
+testImage = downloadImage.retrieveImage(11)
+print(testImage)
+testDBCheck = checkDB.checkPickUp()
+print(testDBCheck)
+
+testInfo = retrieveInfo.retrieveCol(11, 'user')
+print(testInfo)
+
+testInfo = updateStatus.updateStatus(11, 'PyCharm Conversion')
+print(testInfo)
+
+#Callable method to retrieve any product ID needing pick up
+'''
+try:
+    while True:
+        testDBCheck = checkDB.checkPickUp() #If no pick up return a False
+        if testDBCheck == False:
+            print('No Cube incoming request')
+        else: 
+            print(testDBCheck)
+            #Callable method to retrieve an image by Primary Key Insert where needed
+            testImage = downloadImage.retrieveImage(testDBCheck)
+            print(testImage)
+            
+            
+            #Test Sequence to retrieve column data from database
+            testInfo = retrieveInfo.retrieveCol(testDBCheck, 'user')
+            print(testInfo)
+            
+            
+            #Test status Update
+            testInfo = updateStatus.updateStatus(testDBCheck,'Cube Assigned RFID')
+            print(testInfo)
+            time.sleep(5)
+except KeyboardInterrupt:
+    pass
+        
+'''
---
+++ PyCharm_Conversion/downloadImage.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 07:02:29 2021
+
+@author: georg
+"""
+#Imports necessary packages
+import mysql.connector
+import sshtunnel
+import requests
+import os
+
+#Defines the function to call outside of the system
+def retrieveImage(Id):
+    #Establishes secure tunnel connection config settings
+    sshtunnel.SSH_TIMEOUT = 5.0
+    sshtunnel.TUNNEL_TIMEOUT = 5.0
+    
+    #Connects with database through SSH tunnel hosted on public Linux Setup
+    try:
+        with sshtunnel.SSHTunnelForwarder(
+            ('ssh.pythonanywhere.com'),
+            ssh_username='jgbroz', ssh_password='Sly!25321',
+            remote_bind_address=('jgbroz.mysql.pythonanywhere-services.com', 3306)
+        ) as tunnel:
+            db = mysql.connector.connect(
+                user='jgbroz', password='Acs3302021',
+                host='127.0.0.1', port=tunnel.local_bind_port,
+                database='jgbroz$CentralSystem',
+            )
+            
+            #Change Path when integrating
+            savePath = '/home/jgbroz/Documents/CS/PyCharm_Conversion/Retrieved Images'
+            
+            #Saves the passed in Id
+            productID = Id
+
+            for x in range(1,7):
+                #Begin Query Process
+                mycursor = db.cursor()
+                #select image Column from Database Table
+                query = "SELECT img{} FROM productDetails WHERE productID = {}".format(x,productID) 
+                mycursor.execute(query)
+                
+                #Store the first result in a variable
+                row = mycursor.fetchone()
+                splitRow = row[0].split(".")
+                print(splitRow[0])
+                print(splitRow[1])
+                fileName = "{}_{}_{}.{}".format(productID,splitRow[0],x,splitRow[1])
+                print(fileName)
+                #Join File paths to one another when saving image
+                completePath = os.path.join(savePath, fileName)
+                print(completePath)
+                #Join Image name to expected url to access it
+                setUrl = 'http://jgbroz.pythonanywhere.com/uploads/'+ row[0]
+                
+                #print (setUrl) Prints the full URL (Testing)
+                #Downlaods image using Requests tree
+                r = requests.get(setUrl, allow_redirects=True)
+                if r.status_code == 200:
+                    print('Image retrieved')
+                #Opens a new file to save the data to.
+                open(completePath, 'wb').write(r.content)
+            #Closes the database 
+            db.close()
+            #returns a success if successful
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        return ('Fail')
+    finally:
+        if db:
+            db.close()
+            #returns a success if successful
+            return ('Success')
+        print('All Connections closed')
+        return ('Success')
